778b4e853bc834c195126034c79fccfd1f912ba3
Lexiie <lexiie.casanova@gmail.com>
2017-05-06 02:30:52 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -130,7 +130,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://www.penyon.net/p/safelink.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://dl4penyon.blogspot.com/p/safelink.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
