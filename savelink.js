3f9600ec1c42591afdcb6676f4418d770297f2c4
Lexiie <lexiie.casanova@gmail.com>
2017-06-21 14:15:39 +0800

    Update savelink.js

--- savelink.js
+++ savelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://www.penyon.net/p/go.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "http://www.penyon.net/p/blog_page.20.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
